f3f4cdc30476bde8d04feab47d88c065fc52136a
Sayem <sayem465508@gmail.com>
2023-12-05 09:55:19 +0600

    dynamicaly loaded data from api

--- js/index.js
+++ js/index.js
@@ -25,34 +25,34 @@ const handleNews = async (categoryId) => {
     const data = await response.json();
     
     const cardContainer = document.getElementById('card-container')
+    cardContainer.textContent = '';
     data.data.forEach((news) => {
+        console.log(news)
         const div  = document.createElement('div');
         div.innerHTML = `
 
         <div class="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <figure><img src="${news?.image_url}" alt="Shoes" /></figure>
             <div class="">
               
               <div class="flex items-center mt-5 px-5">
-                <h2 class="text-2xl font-semibold">Biden Pledges Nearly $3 Billion To Ukraine</h2>
-                <button class="btn btn-secondary rounded-3xl">Excellent</button>
+                <h2 class="text-2xl font-semibold">${news?.title.slice(0, 40)
+                }...</h2>
+                <button class="btn btn-secondary rounded-3xl">${news?.rating?.badge}</button>
               </div>
-              <p class="px-5 mt-4">Wednesday, August 24, 2022 | Tag Cloud<br>
-                  Tags: Biden, EU, Euro, Europe, Joe Biden
-            
-                </p>
+              <p class="px-5 mt-4">${news?.details.slice(0, 80)}</p>
             </div>
 
             <div class="px-5 flex items-center justify-between pb-5">
                 <div class="flex gap-1 items-center mt-10 ">
                     <div class="avatar online">
                         <div class="w-16 rounded-full">
-                          <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                          <img src="${news?.author?.img}" />
                         </div>
                     </div>
                     <div class="space-y-2">
-                        <h3 class="text-xl font-medium">Jimmy Dane</h3>
-                        <p>2022-08-24 17:27:34</p>
+                        <h3 class="text-xl font-medium">${news?.author?.name}</h3>
+                        <p>${news?.author?.published_date}</p>
                     </div>
                 </div>
 
